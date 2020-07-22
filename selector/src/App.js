4cbe8c5e86c87b49ce6b988b66b24c6792734ea9
lingFun <49457941+lingFun@users.noreply.github.com>
2020-07-22 15:17:43 -0400

    completed workshop2

--- selector/src/App.js
+++ selector/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Shape from './Shape'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class Selector extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        selectedShape: 'square',
+        time: 0,
+    };
+  };
+
+  render() {
+    return (
+      <div className="container"> 
+        <div className="navbar">
+          <div>Selected: </div>
+          <div>{this.state.selectedShape}</div>
+        </div>
+        <div className="shape-list">
+          <Shape shape="square" selectShape={this.selectShape}/>
+          <Shape shape="circle" selectShape={this.selectShape}/>
+          <Shape shape="oval" selectShape={this.selectShape}/>
+          <Shape shape="triangle" selectShape={this.selectShape}/>
+          <Shape shape="triangle_down" selectShape={this.selectShape}/>
+          <Shape shape="triangle_left" selectShape={this.selectShape}/>
+          <Shape shape="triangle_right" selectShape={this.selectShape}/>
+          <Shape shape="trapezium" selectShape={this.selectShape}/>
+          <Shape shape="star" selectShape={this.selectShape}/>
+        </div>
+        <div className="footer">{this.state.time} shapes has been clicked.</div>
+      </div>
+    )
+  }
+
+  selectShape = (shapeName) => {
+    this.setState({
+      selectedShape: shapeName,
+      time: this.state.time + 1
+    })
+  }
 }
 
-export default App;
+export default Selector
