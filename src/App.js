ffa843c8c991684cb9acb8bd9fa76c90d1f70dff
lingFun <49457941+lingFun@users.noreply.github.com>
2020-07-22 12:25:41 -0400

    completed workshop1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      unit: 1,
+    };
+  }
+
+  increment = () => {
+    this.setState ({
+      count:this.state.count<20?this.state.count + this.state.unit:20
+    })
+  } 
+
+  decrement = () => {
+    this.setState ({
+      count:this.state.count>0?this.state.count - this.state.unit:0
+    })
+  }
+
+  clear = () => {
+    this.setState ({
+      count: 0
+    })
+  }
+
+  toggle = () => {
+    this.setState ({
+      unit:this.state.unit===1?2:1
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="navbar">Counter.js</div>
+        <div className="counter">
+          <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.increment}>Increment</button>
+          <button type="button" onClick={this.decrement}>Decrement</button>
+          <button type="button" onClick={this.clear}>Clear</button>
+    <button type="button" onClick={this.toggle}>Unit: {this.state.unit}</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Counter;
+
